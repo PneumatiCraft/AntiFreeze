4d37bbddc0beb5576ac1a701293eff76ba2edc5c
Tim Ekl <lithium3141@gmail.com>
2012-01-23 22:54:41 -0500

    First commit; stub out plugin
    Signed-off-by: Tim Ekl <lithium3141@gmail.com>

---
+++ src/main/java/com/pneumaticraft/antifreeze/AntiFreeze.java
@@ -0,0 +1,15 @@
+package com.pneumaticraft.antifreeze;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+class AntiFreeze extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        
+    }
+
+    @Override
+    public void onDisable() {
+        
+    }
+}
